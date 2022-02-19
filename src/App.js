7124c8de85a24f87c00ef690dc73ebdd8d87ee93
Shane McDonald <shanemcdonald@Shanes-Air.attlocal.net>
2022-02-18 21:43:54 -0600

    setting up practice app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SimpleInput from "./Components/SimpleInput";  
+
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleInput />
+    </div>
+  );
+}
+
+export default App;
