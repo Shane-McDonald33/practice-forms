1864fba46bc1147206b2c7143615a497f427acdf
Shane McDonald <shanemcdonald@Shanes-Air.attlocal.net>
2022-02-20 11:31:02 -0600

    about to downsize and clean the code

--- src/Components/SimpleInput.jsx
+++ src/Components/SimpleInput.jsx
@@ -1,15 +1,34 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useEffect} from "react";
+
 
 
 const SimpleInput = (props) => {
-    const nameInputRef = useRef();
+    //const nameInputRef = useRef();
     const [enteredName, setEnteredName] = useState('');
     const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
     const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
+    useEffect(() => {
+        if (enteredNameIsValid) {
+            console.log('Name Is Valid')
+        }
+    }, [enteredNameIsValid])
+
     const nameInputChangeHandler = (event) => {
-        setEnteredName(event.target.value)
+        setEnteredName(event.target.value);
+        if (event.target.value.trim() !== '') {//FORM VALIDATION, this if check sees that the input field has characters entered into the field, 'trim' gets rid of whitespace,
+            //and if the enteredName state is empty, then it returns back to preventDefault and the app won't move forward
+            setEnteredNameIsValid(true);
+        }
     };
+
+    const nameInputBlurHandler = () => {
+        setEnteredNameTouched(true);
+        if (enteredName.trim() === '') {//FORM VALIDATION, this if check sees that the input field has characters entered into the field, 'trim' gets rid of whitespace,
+            //and if the enteredName state is empty, then it returns back to preventDefault and the app won't move forward
+            setEnteredNameIsValid(false);
+        }
+    }
 
     const formSubmissionHandler = (event) => {
         event.preventDefault();
@@ -21,8 +40,8 @@ const SimpleInput = (props) => {
         }
         setEnteredNameIsValid(true);
         console.log(enteredName)//checking state output
-        const enteredValue = nameInputRef.current.value;//this just sets state/informs the console what the state is
-        console.log(enteredValue);
+        // const enteredValue = nameInputRef.current.value;//this just sets state/informs the console what the state is
+        // console.log(enteredValue);
         //nameInputRef.current.ref = ''//this will also clear input field, but it is bad practice and not recommended
         setEnteredName('');//clears the input field by using state
 
@@ -38,7 +57,7 @@ const SimpleInput = (props) => {
         <form onSubmit={formSubmissionHandler}>
             <div className={nameInputClasses}>
                 <label htmlFor="name">Your Name</label>
-                <input ref={nameInputRef} type='text' id="name" onChange={nameInputChangeHandler} value={enteredName}/>
+                <input type='text' id="name" onChange={nameInputChangeHandler} value={enteredName} onBlur={nameInputBlurHandler}/>
                 {nameInputIsInvalid && <p className="error-text">Name Must Not Be Empty</p>}
             </div>
             <div className="form-actions">
